ddd66ce37a2c19390bfe9398f88c4efc05d3ddea
Daniela Cayuela T <daniela.cayuela.tovar@gmail.com>
2021-07-04 20:34:45 -0500

    removed

--- src/Reducer/index.js
+++ src/Reducer/index.js
@@ -6,14 +6,12 @@ const initialState = {
 export function reducer(state = initialState, action) {
   switch (action.type) {
     case "GET_PARKINGS":
-      console.log("ENTRE REDUCER");
       return {
         ...state,
         parkings: action.payload,
         noResults: false,
       };
     case "NO_RESULTS":
-      console.log("ENTRE REDUCER");
       return {
         ...state,
         noResults: action.payload,
